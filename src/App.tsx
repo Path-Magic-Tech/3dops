b17b7595ed86449382ab21a0fa2c37dc10b36ce3
con-sou <conorsouhrada@icloud.com>
2025-07-11 12:28:49 -0600

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,7 @@ import Military from './Military'
 function App() {
   return (
     <>
-      <div>
-        <Military></Military>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Military />
     </>
   )
 }
